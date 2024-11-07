56e1d29297b0a448f44d75913025e2fa19bafb86
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-11-07 12:52:26 +0300

    user(register-login) controller and page added tested.

---
+++ BackendProject/BackendProject/Controllers/UserController .cs
@@ -0,0 +1,48 @@
+﻿using BackendProject.Models;
+using BackendProject.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProject.Controllers;
+
+public class UserController(IUserRepository _userRepository) : Controller
+{
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            var isRegistered = await _userRepository.RegisterUser(user);
+            if (isRegistered)
+                return RedirectToAction("Login");
+
+            ModelState.AddModelError("", "Bu e-posta ile kayıtlı bir kullanıcı zaten var.");
+        }
+        return View(user);
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        var user = await _userRepository.LoginUser(email, password);
+        if (user != null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        ModelState.AddModelError("", "Geçersiz e-posta veya şifre.");
+        return View();
+    }
+}
